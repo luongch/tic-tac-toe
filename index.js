b06969d0324615fe2e1c30e89803815fae0308f3
Chris Luong <christopher_luong@live.ca>
2022-07-09 13:30:27 -0400

    update grid creation to give squares row and col data attributes

--- index.js
+++ index.js
@@ -1,48 +1,71 @@
 const board = (() => {
-    const gridSize = 9;
-    let board = [];
-
+    const gridSize = 3;
+    let board = [[],[],[]];
+    console.log(board)
     const createBoard = () => {
         let container = document.querySelector(".container");
-        let row = createRow();
+        
 
         for(let i = 0; i<gridSize; i++) {
-            let square = createSquare(i);
-            row.appendChild(square)
-
-            if (row.childNodes.length == 3) {
-                container.appendChild(row);
-                row = createRow();
-            }
+            let row = createRow(i);
+            container.appendChild(row)
         }
     }
 
-    const createRow = () => {
+    const createRow = (rowIndex) => {
         let row = document.createElement('div');
         row.classList.add('row');
+        for(let i = 0; i<gridSize; i++) {
+            let square = createSquare(rowIndex, i);
+            row.appendChild(square)
+        }
+        
         return row;
     }
 
-    const createSquare = (i) => {
+    const createSquare = (rowIndex, colIndex) => {
         let square = document.createElement('div');
-        square.dataset.squareId = i;
+        square.dataset.row = rowIndex;
+        square.dataset.col = colIndex;
         square.classList.add('square')
 
-        square.addEventListener('click', function() {
-            placeMark();
+        square.addEventListener('click', function(e) {
+            placeMark(e);
+            updateBoard(e);
         })
 
         return square;
     }
 
+    const updateBoard = (e) => {
+
+        let rowIndex = parseInt(e.target.dataset.row)
+        let colIndex = parseInt(e.target.dataset.col)
+        board[rowIndex][colIndex] = e.target.innerHTML;
+    }
+
     return {createBoard};
 })();
 
+const displayController = (()=>{
+    let player = true;
+
+    placeMark = (e) => {
+        let index = parseInt(e.target.dataset.squareId);
+        e.target.innerHTML = player ? 'o' : 'x';
+        player = !player;
+    }
+})();
+
+const playerFactory = (name, number) => {
+    return {};
+}
 
 
 
 board.createBoard();
 
-placeMark = () => {
-    console.log("worked")
-}
+
+let playerOne = playerFactory("A", 1);
+let playerTwo = playerFactory("B", 2);
+
